bbd0a2503c2640008857eada899cdb8a6c9b5b9e
Szabo Gergely <gege251@mailbox.org>
2025-02-19 14:38:21 +0100

    Remove darwin CI builder

--- flake.nix
+++ flake.nix
@@ -107,7 +107,7 @@
 
       devShell = perSystem (system: self.flake.${system}.devShell);
 
-      herculesCI.ciSystems = [ "x86_64-linux" "x86_64-darwin" ];
+      herculesCI.ciSystems = [ "x86_64-linux" ];
     };
 }
 
